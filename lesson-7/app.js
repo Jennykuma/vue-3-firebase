845d28a7bf436ff2b33e010357b16c38fc3716c5
Jenny <jenny.le@ucalgary.ca>
2021-02-05 18:11:48 -0700

    Add lesson 7 material

---
+++ lesson-7/app.js
@@ -0,0 +1,7 @@
+const app = Vue.createApp({
+  // can add data, functions to react to events we want
+  // can add template we want to render in the app element
+  // template: '<h2> I am the template! </h2>'
+})
+
+app.mount('#app')
