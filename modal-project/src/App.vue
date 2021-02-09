d34092c11bfe4ad357e9d1705ec9bd3d7c778547
Jenny <jenny.le@ucalgary.ca>
2021-02-08 20:08:16 -0700

    Multiple Components

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <h1>{{ title }}</h1>
-
-  <!-- Template Refs allow us to store a refernece to a DOM element inside a variable -->
-  <!-- Then when we have that reference, we can use regular JS methods + properties on it to change classes, text content, styles, etc -->
-  <input text="text" ref="name">
-  <button @click="handleClick">Click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal'
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
       title: 'My First Vue App :)'
-    }
-  },
-  methods: {
-    handleClick() {
-      console.log(this.$refs.name) // Gives a handle on the <input> field
-      this.$refs.name.classList.add('active') // Add the class 'active' onto the input element
-      this.$refs.name.focus() // Focuses on the input element
     }
   }
 }
