6446ddfc59d8540f77d4374cc97329c7ac3ee226
Jenny <jenny.le@ucalgary.ca>
2021-02-08 19:57:50 -0700

    Template Refs

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>{{ title }}</h1>
+
+  <!-- Template Refs allow us to store a refernece to a DOM element inside a variable -->
+  <!-- Then when we have that reference, we can use regular JS methods + properties on it to change classes, text content, styles, etc -->
+  <input text="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
@@ -8,6 +13,13 @@ export default {
   data() {
     return {
       title: 'My First Vue App :)'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name) // Gives a handle on the <input> field
+      this.$refs.name.classList.add('active') // Add the class 'active' onto the input element
+      this.$refs.name.focus() // Focuses on the input element
     }
   }
 }
