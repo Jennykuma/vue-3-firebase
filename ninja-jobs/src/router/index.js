ed39ac8a0cb2165a7ccbfdc0271c70213e910e7e
Jenny <jenny.le@ucalgary.ca>
2021-02-13 15:32:07 -0700

    Router Setup for New Projects

---
+++ ninja-jobs/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL), // use the web history api in the browser to go back to previous routes (forward + back)
+  routes
+})
+
+export default router
