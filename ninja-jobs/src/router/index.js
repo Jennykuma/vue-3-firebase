d2a2de351c99d2c95f57ddfe9f5ecaf00de2000e
Jenny <jenny.le@ucalgary.ca>
2021-02-15 14:01:33 -0700

    404 Pages & Redirects

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
 
@@ -25,6 +26,17 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs', // old path
+    redirect: '/jobs' // redirects to this path
+  },
+  // catchall 404 ----v (has to be specific like this)
+  {
+    path: '/:catchAll(.*)', // catch any route that is not caught by those listed above
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
