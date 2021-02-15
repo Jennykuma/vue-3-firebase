391cff643b65c2f8c09608fdcfcb0d1ba1871424
Jenny <jenny.le@ucalgary.ca>
2021-02-14 20:06:29 -0700

    Route Parameters

---
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Job Details Page</h1>
+
+  <!-- jobs/:id will be inside $route.params -->
+  <p>The job id is {{ $route.params.id }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id // use 'this' to reference the component
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
