d8b0940cb96fd9206ddcc5f96266f76b18150317
Jenny <jenny.le@ucalgary.ca>
2021-02-14 20:39:07 -0700

    Dynamic Links

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -2,16 +2,12 @@
   <h1>Job Details Page</h1>
 
   <!-- jobs/:id will be inside $route.params -->
-  <p>The job id is {{ $route.params.id }}</p>
+  <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      id: this.$route.params.id // use 'this' to reference the component
-    }
-  }
+  props: ['id'] // we are now accepting the params: { id: .. } as a prop, so no need for this.$routes.params.id
 }
 </script>
 
