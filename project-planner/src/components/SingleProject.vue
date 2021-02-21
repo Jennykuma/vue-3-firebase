9d46f7eb90e942637fb0d5f7b89cd7788ea5aff0
Jenny <jenny.le@ucalgary.ca>
2021-02-20 18:50:59 -0700

    Completing Projects

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="project">
+  <!-- Will get the 'complete' class if project.complete is true -->
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="toggleDetails">{{ project.title }}</h3>
       <div class="icons">
         <span class="material-icons">edit</span>
         <span @click="deleteProject" class="material-icons">delete</span>
-        <span class="material-icons">done</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
       </div>
     </div>
     <div v-if="showDetails" class="details">
@@ -33,6 +34,15 @@ export default {
       fetch(this.uri, { method: 'DELETE' })
         .then(() => this.$emit('delete', this.project.id))
         .catch(err => console.log(err))
+    },
+    toggleComplete() {
+      fetch(this.uri, { 
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' }, // Sending data of type JSON with this request
+        body: JSON.stringify({ complete: !this.project.complete }) // Toggle the completion of the project. Has to be a JSON string!
+      }).then(() => {
+        this.$emit('complete', this.project.id)
+      }).catch((err) => console.log(err))
     }
   }
 }
@@ -63,5 +73,12 @@ export default {
   }
   .material-icons:hover {
     color: #777;
+  }
+  /* completed projects */
+  .project.complete {
+    border-left: 4px solid #00ce89;
+  }
+  .project.complete .tick {
+    color: #00ce89;
   }
 </style>
--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -3,7 +3,7 @@
     <!-- Will show if there are entries in projects -->
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" />
+        <SingleProject :project="project" @delete="handleDelete"  @complete="handleComplete" />
       </div>
     </div>
   </div>
@@ -32,6 +32,13 @@ export default {
       this.projects = this.projects.filter((project) => {
         return project.id !== id
       })
+    },
+    handleComplete(id) {
+      // Update the local data of the chosen project
+      let p = this.projects.find(project => {
+        return project.id === id
+      })
+      p.complete = !p.complete
     }
   }
 }
