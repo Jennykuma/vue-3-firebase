3447e0829bbb3bc28da2d5643be5e54aa801af5b
Jenny <jenny.le@ucalgary.ca>
2021-02-25 21:41:52 -0700

    Using Refs for Reactivity

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -3,6 +3,8 @@
     Home
     <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
@@ -12,23 +14,20 @@ import { ref } from 'vue'
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
-
-    // Ref for paragraph tag
     const p = ref(null)
-    console.log(p, p.value) // Will be null
 
-    let name = 'mario'
-    let age = 30
+    // Just because these are constants doesn't mean the values can't change
+    // It means the reference object is a constant, but the v alue inside can change
+    // Access the value ex: name.value, age.value
+    const name = ref('mario')
+    const age = ref(30)
 
     const handleClick = () => {
-      console.log(p, p.value)
-      p.value.classList.add('test')
-      p.value.textContent = 'hello, ninjas'
+      name.value = 'luigi'
+      age.value = 35
     }
 
-    return { name, age, handleClick, p } // Allowing these variables to be accessed inside the template
-    // These variables are not reactive but the data inside data() is reactive
+    return { name, age, handleClick, p }
   }
 }
 </script>
