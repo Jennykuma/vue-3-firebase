98d9c498a49dfd45e1747167f94de9a276ce1435
Jenny <jenny.le@ucalgary.ca>
2021-02-25 21:35:34 -0700

    Template Refs in the Setup Function

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   setup() {
     console.log('setup')
 
+    // Ref for paragraph tag
+    const p = ref(null)
+    console.log(p, p.value) // Will be null
+
     let name = 'mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
-    return { name, age, handleClick } // Allowing these variables to be accessed inside the template
+    return { name, age, handleClick, p } // Allowing these variables to be accessed inside the template
     // These variables are not reactive but the data inside data() is reactive
-  },
-  created() {
-    console.log('created')
-  },
-  mounted() {
-    console.log('mounted')
   }
 }
 </script>
