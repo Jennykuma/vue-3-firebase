d2ea963499882258cf6d73ce6c1a1677ee45630b
Jenny <jenny.le@ucalgary.ca>
2021-03-10 17:48:40 -0700

    Creating a Tag Cloud

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -2,8 +2,9 @@
   <div class="home">
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else>
       <Spinner />
@@ -13,12 +14,13 @@
 
 <script>
 import PostList from '../components/PostList'
+import TagCloud from '../components/TagCloud'
 import getPosts from '../composables/getPosts'
 import Spinner from '../components/Spinner'
 
 export default {
   name: 'Home',
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
 
     // Initially, posts and error will be an empty array and null respectively
@@ -37,5 +39,11 @@ export default {
     max-width: 1200px;
     margin: 0 auto;
     padding: 10px;
+  }
+
+  .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
   }
 </style>
