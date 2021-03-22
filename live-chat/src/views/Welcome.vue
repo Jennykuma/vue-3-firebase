6ab560e4c832a6b35a4b78c069faac35bada73a6
Jenny <jenny.le@ucalgary.ca>
2021-03-22 17:33:34 -0600

    Creating a Signup & Login Form

--- live-chat/src/views/Welcome.vue
+++ live-chat/src/views/Welcome.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <LoginForm />
   </div>
 </template>
 
 <script>
-export default {
+import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
 
+export default {
+  components: { LoginForm }
 }
 </script>
 
@@ -14,5 +18,26 @@ export default {
   .welcome {
     text-align: center;
     padding: 20px 0;
+  }
+
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
   }
 </style>
