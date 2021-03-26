f8aec438abf6271782f6739012733e86a8306911
Jenny <jenny.le@ucalgary.ca>
2021-03-26 17:27:25 -0600

    Challenge - Watching the Current User

--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -5,10 +5,25 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
 import Navbar from '../components/Navbar'
+import getUser from '../composables/getUser'
 
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  setup() {
+    const { user } = getUser()
+    const router = useRouter()
+
+    // watch user
+    // fires the function when user changes
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
+  }
 }
 </script>
 
