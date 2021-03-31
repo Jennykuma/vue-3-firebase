4e45d5cb202dc31568490cb3e5c99434e84fb58b
Jenny <jenny.le@ucalgary.ca>
2021-03-30 19:11:04 -0600

    Paths & Rules Explained

--- live-chat/src/composables/getUser.js
+++ live-chat/src/composables/getUser.js
@@ -3,7 +3,7 @@ import { projectAuth } from '../firebase/config'
 
 const user = ref(projectAuth.currentUser)
 
-// _user is what we get back everytime there is an auth change
+// _user is what we get back everytime there's an auth change
 projectAuth.onAuthStateChanged(_user => {
   console.log('User state change. Current user is: ', _user)
   user.value = _user
