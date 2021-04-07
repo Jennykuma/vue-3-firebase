d6ad4c17efe8172e3940ce7e75d87020d2d15887
Jenny <jenny.le@ucalgary.ca>
2021-04-06 20:40:50 -0600

    Handle File Changes

--- muso-ninjas/src/views/playlists/CreatePlaylist.vue
+++ muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -5,7 +5,9 @@
     <textarea required placeholder="Playlist description..." v-model="description"></textarea>
     <!-- Upload playlist image -->
     <label>Upload playlist cover image</label>
-    <input type="file">
+    <input type="file" @change="handleChange">
+    <div class="error">{{ fileError }}</div>
+    
     <div class="error"></div>
     <button>Create</button>
   </form>
@@ -18,12 +20,32 @@ export default {
   setup() {
     const title = ref('')
     const description = ref('')
+    const file = ref(null)
+    const fileError = ref(null)
+
+    // allowed file types
+    const types = ['image/png', 'image/jpeg']
 
     const handleSubmit = () => {
-      console.log(title.value, description.value)
+      if (file.value) {
+        console.log(title.value, description.value, file.value)
+      }
     }
 
-    return { title, description, handleSubmit }
+    const handleChange = (e) => {
+      // e.target is the input field itself, e.target.files is an array of files
+      const selected = e.target.files[0]
+
+      if (selected && types.includes(selected.type)) {
+        file.value = selected
+        fileError.value = null
+      } else {
+        file.value = null
+        fileError.value = 'Please select an image file (png or jpg)'
+      }
+    }
+
+    return { title, description, file, fileError, handleSubmit, handleChange }
   }
 }
 </script>
