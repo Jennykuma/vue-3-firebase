e8a2994bfe807b465ef1231f03d4076bc5c807df
Jenny <jenny.le@ucalgary.ca>
2021-04-10 20:39:49 -0600

    Add playlist details

--- muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -1,10 +1,28 @@
 <template>
-  <h2>Playlist Details. ID - {{ id }}</h2>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="document" class="playlist-details">
+    <!-- playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="document.coverUrl">
+      </div>
+      <h2>{{ document.title }}</h2>
+      <p class="username">Created by {{ document.userName }}</p>
+      <p class="description">{{ document.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import getDocument from '@/composables/getDocument'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  setup(props) {
+    const { error, document } = getDocument('playlists', props.id)
+
+    return { error, document }
+  }
 }
 </script>
 
